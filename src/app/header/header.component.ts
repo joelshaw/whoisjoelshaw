ec3f4acbc7c538c91d14c4936dd1e056ec016335
joelshaw <shawji@me.com>
2020-04-25 19:51:36 -0400

    added animation module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('showIntro', [
+      state('show', style({
+        opacity: 1
+      })),
+      transition('* => show', animate('2s')),
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
@@ -11,6 +20,12 @@ export class HeaderComponent implements OnInit {
   subtitle = 'Software Engineer & Photographer';
 
   constructor() { }
+
+  isShown = false;
+
+  toggle() {
+    this.isShown = !this.isShown;
+  }
 
   ngOnInit() {
   }
