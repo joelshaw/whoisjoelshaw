d6cdb537186d9c8a6d77c0e50690352d71ac0e16
joelshaw <shawji@me.com>
2020-07-21 11:38:17 -0400

    Created new Google Analytics service

--- src/app/google-analytics.service.ts
+++ src/app/google-analytics.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+declare let ga;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,4 +9,15 @@ export class GoogleAnalyticsService {
 
   constructor() {}
 
+  public eventEmitter(eventCategory: string,
+                      eventAction: string,
+                      eventLabel: string = null,
+                      eventValue: number = null) {
+      ga('send', 'event', {
+        eventCategory: eventCategory,
+        eventLabel: eventLabel,
+        eventAction: eventAction,
+        eventValue: eventValue
+      });
+    }
 }
