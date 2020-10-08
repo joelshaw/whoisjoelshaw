209b2328c03b4a9773257b63e7ea5914f96d2a8c
joelshaw <shawji@me.com>
2020-10-08 16:50:05 -0400

    Created photography component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { GoogleAnalyticsService } from './google-analytics.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PhotographyComponent } from './photography/photography.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PhotographyComponent
   ],
   imports: [
     BrowserModule,
