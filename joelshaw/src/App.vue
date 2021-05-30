63a9c8101dc73115fbaa6b8a1a61c5370a10c12f
joelshaw <shawji@me.com>
2021-05-29 20:49:53 -0500

    added font pack, intro component design

--- joelshaw/src/App.vue
+++ joelshaw/src/App.vue
@@ -16,5 +16,22 @@ export default {
 </script>
 
 <style>
+* {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
+body {
+  margin: 0;
+  padding: 0;
+  background: #D9CEB6;
+  text-transform: uppercase;
+  font-family: zeitung, sans-serif;
+  font-weight: 100;
+  font-style: normal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 </style>
--- joelshaw/src/components/Introduction.vue
+++ joelshaw/src/components/Introduction.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="introduction">
-    Introduction
+    <div class="introduction__textBlock">
+      <h1>Joel Isaac Shaw</h1>
+      <h2>Software Engineer + Photographer</h2>
+      <h3>Nashville, TN</h3>
+    </div>
   </div>
 </template>
 
@@ -15,5 +19,47 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .introduction {
+    background: #333333;
+    color: #D9CEB6;
+    text-align: center;
+    letter-spacing: 3.25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 95vw;
+    height: 93vh;
+  }
 
+  .introduction h1 {
+    letter-spacing: 0.125em;
+    font-size: 3.25em;
+    width: 100%;
+  }
+
+  .introduction h1:after {
+display: block;
+    content: '';
+    width: 30%;
+    border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+    margin: 0 auto;
+    position: relative;
+    top: 30px;
+  }
+
+  .introduction h2 {
+    margin: 1.25em 0 0.75em 0;
+    font-size: 1.6em;
+    width: 100%;
+  }
+
+  .introduction h3 {
+    font-size: 1.125em;
+    width: 100%;
+  }
+
+  .introduction .introduction__textBlock {
+    display: flex;
+    flex-wrap: wrap;
+  }
 </style>
